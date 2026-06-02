c03f062d39b4bf96a2c9bd41836dbed6e318cfef
bunny <bunnytns6@gmail.com>
2026-06-02 18:01:46 +0530

    Known React

---
+++ newapp/src/components/HomePage/index.js
@@ -0,0 +1,10 @@
+import "./index.css"
+import {Heading} from "./styledComponents"
+const Home =() =>(
+    <div>
+        <h1 className="hed">Home</h1>
+        <Heading color="red">Hello</Heading>
+    </div>
+)
+
+export default Home
